3094992c5150c7201db13b6e3a6ae02711519e7f
Darkhhh <a.a.mikov@mail.ru>
2024-03-14 12:32:54 +0300

    Add filters registration to IWorld

--- RelatedECS/IWorld.cs
+++ RelatedECS/IWorld.cs
@@ -1,6 +1,9 @@
-﻿namespace RelatedECS;
+﻿using RelatedECS.Filters;
+
+namespace RelatedECS;
 
 public interface IWorld
 {
-
+    public IEntitiesFilter RegisterAsEntitiesFilter(IFilterDeclaration declaration);
+    public IIndicesFilter RegisterAsIndicesFilter(IFilterDeclaration declaration);
 }
