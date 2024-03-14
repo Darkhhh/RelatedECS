a4deac91a3e0b7436ab59d6896d399d68e31e1ff
Darkhhh <a.a.mikov@mail.ru>
2024-03-14 11:34:40 +0300

    Improvements for SharedBag.cs

--- RelatedECS/Maintenance/Utilities/SharedBag.cs
+++ RelatedECS/Maintenance/Utilities/SharedBag.cs
@@ -20,6 +20,9 @@ public class SharedBag : ISharedBag
     private readonly Dictionary<Type, object> _objects = new();
     private readonly Dictionary<string, object> _taggedObjects = new();
 
+    public int Count => _objects.Count;
+    public int CountTagged => _taggedObjects.Count;
+
     public void Add<T>(T obj) where T : notnull
     {
         var type = typeof(T);
@@ -36,14 +39,14 @@ public class SharedBag : ISharedBag
     public T Get<T>() where T : notnull
     {
         var type = typeof(T);
-        if (!_objects.ContainsKey(type)) throw new Exception($"Object of type {type.Name} does not exist in bag");
-        return (T)_objects[type];
+        if (!_objects.TryGetValue(type, out var value)) throw new Exception($"Object of type {type.Name} does not exist in bag");
+        return (T)value;
     }
 
     public T Get<T>(string tag) where T : notnull
     {
-        if (!_taggedObjects.ContainsKey(tag)) throw new Exception($"Object with tag {tag} does not exist in bag");
-        return (T)_taggedObjects[tag];
+        if (!_taggedObjects.TryGetValue(tag, out var value)) throw new Exception($"Object with tag {tag} does not exist in bag");
+        return (T)value;
     }
 
     public bool Has<T>() where T : notnull
