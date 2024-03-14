a30f399fc1fb2b5aabf72e66b1999f3bac06cfa0
Darkhhh <a.a.mikov@mail.ru>
2024-03-14 12:48:27 +0300

    ObjectPool improvements.

--- RelatedECS/Maintenance/Utilities/ObjectPool.cs
+++ RelatedECS/Maintenance/Utilities/ObjectPool.cs
@@ -8,10 +8,15 @@ public interface IAutoReset
     public void Reset();
 }
 
+public interface IObjectPool
+{
+    public Type GetObjectType();
+}
+
 /// <summary>
 /// Based on <see href="https://learn.microsoft.com/ru-ru/dotnet/standard/collections/thread-safe/how-to-create-an-object-pool">Microsoft Documentation</see>
 /// </summary>
-public class ObjectPool<T> where T : IAutoReset, new()
+public class ObjectPool<T> : IObjectPool where T : IAutoReset, new()
 {
     private readonly ConcurrentBag<T> _objects;
     private readonly Func<T> _objectGenerator;
@@ -24,7 +29,7 @@ public class ObjectPool<T> where T : IAutoReset, new()
 
     public T Get()
     {
-        var result = _objects.TryTake(out T item) ? item : _objectGenerator();
+        var result = _objects.TryTake(out var item) ? item : _objectGenerator();
         result.Init();
         return result;
     }
@@ -34,4 +39,8 @@ public class ObjectPool<T> where T : IAutoReset, new()
         item.Reset();
         _objects.Add(item);
     }
+
+    public Type GetObjectType() => typeof(T);
+
+    public int CurrentPoolCapacity => _objects.Count;
 }
