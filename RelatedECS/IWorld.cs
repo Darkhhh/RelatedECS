31bc715bddc3b03c5099acafc73b192fd21ad563
Darkhhh <a.a.mikov@mail.ru>
2024-03-15 14:42:39 +0300

    Add IMessageBus and ISharedBag properties to IWorld.

--- RelatedECS/IWorld.cs
+++ RelatedECS/IWorld.cs
@@ -1,9 +1,14 @@
 ﻿using RelatedECS.Filters;
+using RelatedECS.Maintenance.Utilities;
 
 namespace RelatedECS;
 
 public interface IWorld
 {
+    public IMessageBus Bus { get; }
+
+    public ISharedBag Bag { get; }
+
     public IEntitiesFilter RegisterAsEntitiesFilter(IFilterDeclaration declaration);
     public IIndicesFilter RegisterAsIndicesFilter(IFilterDeclaration declaration);
 }
