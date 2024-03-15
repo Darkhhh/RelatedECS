9a051383e955752f0dec96b2857468fc443e02c4
Darkhhh <a.a.mikov@mail.ru>
2024-03-15 14:42:58 +0300

    Tests. Update WorldDummy.

--- RelatedECS.Tests/Dummies/WorldDummy.cs
+++ RelatedECS.Tests/Dummies/WorldDummy.cs
@@ -1,9 +1,14 @@
 ﻿using RelatedECS.Filters;
+using RelatedECS.Maintenance.Utilities;
 
 namespace RelatedECS.Tests.Dummies;
 
 internal class WorldDummy : IWorld
 {
+    public IMessageBus Bus { get; set; }
+
+    public ISharedBag Bag { get; set; }
+
     public IEntitiesFilter RegisterAsEntitiesFilter(IFilterDeclaration declaration)
     {
         return null;
