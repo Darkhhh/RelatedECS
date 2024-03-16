1fb28ac0228bb776453decc03312fc5e69d12ee3
Darkhhh <a.a.mikov@mail.ru>
2024-03-16 10:38:04 +0300

    Add controller for ComponentsPool

---
+++ RelatedECS/Pools/ComponentsPoolsController.cs
@@ -0,0 +1,30 @@
+﻿namespace RelatedECS.Pools;
+
+public class ComponentsPoolsController(PoolUpdated action) : IComponentsPoolsController
+{
+    private readonly Dictionary<Type, IComponentsPool> _pools = new();
+
+    public int PoolsCount => _pools.Count;
+
+    public PoolUpdated PoolUpdated => action;
+
+    public IReadOnlyList<IComponentsPool> GetAll() => _pools.Values.ToList();
+
+    public ComponentsPool<T> GetPool<T>() where T : struct
+    {
+        var type = typeof(T);
+        if (_pools.TryGetValue(type, out var value)) return (ComponentsPool<T>)value;
+
+        var pool = new ComponentsPool<T>(_pools.Count, PoolBeenChanged);
+        _pools.Add(type, pool);
+        return pool;
+    }
+
+    public IComponentsPool GetPool(Type type)
+    {
+        if (!_pools.TryGetValue(type, out var value)) throw new Exception("Pool is not registered");
+        return value;
+    }
+
+    private void PoolBeenChanged(Type poolType, int entity, bool added) => PoolUpdated(poolType, _pools[poolType].Id, entity, added);
+}
---
+++ RelatedECS/Pools/IComponentsPoolsController.cs
@@ -0,0 +1,16 @@
+﻿namespace RelatedECS.Pools;
+
+public delegate void PoolUpdated(Type poolType, int poolIndex, int entity, bool added);
+
+public interface IComponentsPoolsController
+{
+    public int PoolsCount { get; }
+
+    public PoolUpdated PoolUpdated { get; }
+
+    public ComponentsPool<T> GetPool<T>() where T : struct;
+
+    public IComponentsPool GetPool(Type type);
+
+    public IReadOnlyList<IComponentsPool> GetAll();
+}
