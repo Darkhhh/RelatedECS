08a2c3e5fccd2a59524864c4eaa0ffc5daacfa7a
Darkhhh <a.a.mikov@mail.ru>
2024-03-19 11:34:09 +0300

    Add filter delayed operation struct.

---
+++ RelatedECS/Maintenance/Utilities/DelayedFilterOperation.cs
@@ -0,0 +1,22 @@
+﻿using RelatedECS.Entities;
+
+namespace RelatedECS.Maintenance.Utilities;
+
+internal enum FilterOperationType
+{
+    Add, Remove
+}
+
+internal readonly struct DelayedFilterOperation
+{
+    public readonly Entity Reference;
+    public readonly int EndityId;
+    public readonly FilterOperationType Operation;
+
+    public DelayedFilterOperation(Entity reference, int endityId, FilterOperationType operation)
+    {
+        Reference = reference;
+        EndityId = endityId;
+        Operation = operation;
+    }
+}
