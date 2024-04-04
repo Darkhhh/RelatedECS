a2b29e83467e1999b56a1d7dc97e8d132386ce7d
Darkhhh <a.a.mikov@mail.ru>
2024-04-04 12:03:08 +0300

    Update IWorld

--- RelatedECS/IWorld.cs
+++ RelatedECS/IWorld.cs
@@ -2,19 +2,41 @@
 using RelatedECS.Filters;
 using RelatedECS.Maintenance.Utilities;
 using RelatedECS.Pools;
+using RelatedECS.Systems.SystemGroups;
+using RelatedECS.Systems;
 
 namespace RelatedECS;
 
 public interface IWorld
 {
+    public IWorld AddSystem(ISystem system);
+
+    public IWorld AddGroup(ISystemGroup systemGroup);
+
+    public ISystemGroup GetSystemGroup(string name);
+
+    public IWorld Prepare();
+
+    public void Execute();
+
+    public void Dispose();
+
+    public EntitiesFilter RegisterFilter(IFilterDeclaration declaration);
+
+
     public ComponentsPool<T> GetPool<T>() where T : struct;
 
+    public IComponentsPool GetPool(Type type);
+
+
     public IEntity NewEntity();
+
+    public EntityPack PackEntity(IEntity entity);
+
+    public IEntity GetEntityById(int id);
+
 
     public IMessageBus Bus { get; }
 
     public ISharedBag Bag { get; }
-
-    public IEntitiesFilter RegisterAsEntitiesFilter(IFilterDeclaration declaration);
-    public IIndicesFilter RegisterAsIndicesFilter(IFilterDeclaration declaration);
 }
--- RelatedECS/World.cs
+++ RelatedECS/World.cs
@@ -1,5 +1,105 @@
-﻿namespace RelatedECS;
+﻿using RelatedECS.Entities;
+using RelatedECS.Filters;
+using RelatedECS.Maintenance.Utilities;
+using RelatedECS.Pools;
+using RelatedECS.Systems;
+using RelatedECS.Systems.SystemGroups;
 
-public class World
+namespace RelatedECS;
+
+public class World : IWorld
 {
+    private readonly ComponentsPoolsController _componentsPoolsController;
+    private readonly EntitiesController _entitiesController;
+    private readonly FiltersController _filtersController;
+    private readonly ISystemsController _systemsController;
+    
+    private readonly MessageBus _messageBus;
+    private readonly SharedBag _sharedBag;
+
+    public IMessageBus Bus => _messageBus;
+    public ISharedBag Bag => _sharedBag;
+
+
+    public World()
+    {
+        _componentsPoolsController = new ComponentsPoolsController(PoolUpdated);
+        _entitiesController = new EntitiesController(this);
+        _filtersController = new FiltersController(_entitiesController, _componentsPoolsController);
+        _systemsController = new SystemsController();
+
+        _messageBus = new MessageBus(this);
+        _sharedBag = new SharedBag();
+    }
+
+    public IWorld AddSystem(ISystem system)
+    {
+        _systemsController.AddSystem(system);
+        return this;
+    }
+
+    public IWorld AddGroup(ISystemGroup systemGroup)
+    {
+        _systemsController.AddGroup(systemGroup);
+        return this;
+    }
+
+    public ISystemGroup GetSystemGroup(string name)
+    {
+        return _systemsController.GetSystemGroup(name);
+    }
+
+    public IWorld Prepare()
+    {
+        _systemsController.Prepare(this);
+        return this;
+    }
+
+    public void Execute()
+    {
+        _systemsController.FramePrepare(this);
+        _systemsController.Execute(this);
+        _systemsController.LateExecute(this);
+        _systemsController.FrameDispose(this);
+    }
+
+    public void Dispose()
+    {
+        _systemsController.Dispose(this);
+    }
+
+
+    public EntitiesFilter RegisterFilter(IFilterDeclaration declaration)
+    {
+        var filter = _filtersController.RegisterAsEntitiesFilter(declaration);
+        _entitiesController.UpdatePoolsAmount(_componentsPoolsController.PoolsCount);
+        _filtersController.ResizeMasks(_componentsPoolsController.PoolsCount);
+        return filter;
+    }
+    
+
+    public ComponentsPool<T> GetPool<T>() where T : struct
+    {
+        var pool = _componentsPoolsController.GetPool<T>();
+        _entitiesController.UpdatePoolsAmount(_componentsPoolsController.PoolsCount);
+        _filtersController.ResizeMasks(_componentsPoolsController.PoolsCount);
+
+        return pool;
+    }
+
+    public IComponentsPool GetPool(Type type) => _componentsPoolsController.GetPool(type);
+
+
+    public IEntity NewEntity() => _entitiesController.New();
+
+    public EntityPack PackEntity(IEntity entity) => _entitiesController.Pack(entity);
+
+    public IEntity GetEntityById(int id) => _entitiesController.GetById(id);
+
+
+    private void PoolUpdated(Type poolType, int poolIndex, int entity, bool added)
+    {
+        _entitiesController.PoolUpdated(poolType, poolIndex, entity, added);
+        _filtersController.PoolUpdated(poolType, poolIndex, entity, added);
+    }
 }
