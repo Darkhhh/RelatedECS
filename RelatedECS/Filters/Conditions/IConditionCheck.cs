022b25924b684fd7248df547c46331ca77a44697
Darkhhh <a.a.mikov@mail.ru>
2024-04-08 17:48:52 +0300

    Add Entity components values additionals check mechanism.

---
+++ RelatedECS/Filters/Conditions/IConditionCheck.cs
@@ -0,0 +1,17 @@
+﻿namespace RelatedECS.Filters.Conditions;
+
+public interface IConditionCheck
+{
+    Type ObjectType { get; }
+
+    bool Check(object obj);
+}
+
+abstract class ConditionCheck<T> : IConditionCheck where T : struct
+{
+    public Type ObjectType => typeof(T);
+
+    public bool Check(object obj) => CheckCondition((T)obj);
+
+    public abstract bool CheckCondition(T obj);
+}
---
+++ RelatedECS/Filters/Conditions/IConditionsCheckController.cs
@@ -0,0 +1,42 @@
+﻿using RelatedECS.Pools;
+
+namespace RelatedECS.Filters.Conditions;
+
+public interface IConditionsCheckController
+{
+    public bool AddCheck(IConditionCheck check);
+
+    public void RemoveCheck(IConditionCheck check);
+
+    public void Clear();
+
+    public bool Check(int entity);
+
+    public int Count { get; }
+}
+
+public class ConditionsCheckController(IPoolsProvider controller) : IConditionsCheckController
+{
+    private readonly Dictionary<Type, IConditionCheck> _checks = new();
+    private readonly IPoolsProvider _controller = controller;
+
+    public int Count => _checks.Count;
+
+    public bool AddCheck(IConditionCheck check) => _checks.TryAdd(check.ObjectType, check);
+
+    public void RemoveCheck(IConditionCheck check) => _checks.Remove(check.ObjectType);
+
+    public void Clear() => _checks.Clear();
+
+    public bool Check(int entity)
+    {
+        var flag = true;
+
+        foreach (var check in _checks)
+        {
+            var pool = _controller.GetPool(check.Key);
+            if (!check.Value.Check(pool.GetRaw(entity))) flag = false;
+        }
+        return flag;
+    }
+}
