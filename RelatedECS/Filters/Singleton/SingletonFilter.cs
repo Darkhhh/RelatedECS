9e532a0b08988cd076ff9b08080546cb73490183
Darkhhh <a.a.mikov@mail.ru>
2024-06-22 11:27:35 +0300

    Singleton filter established. Remove .csproj attributes.

---
+++ RelatedECS/Filters/Singleton/SingletonFilter.cs
@@ -0,0 +1,27 @@
+﻿using RelatedECS.Entities;
+
+namespace RelatedECS.Filters.Singleton;
+
+public class SingletonFilter
+{
+    private readonly RegisteredFilter _filter;
+
+    internal IFilterDeclaration Declaration { get; }
+
+    internal RegisteredFilter Raw => _filter;
+
+    internal SingletonFilter(RegisteredFilter filter, IFilterDeclaration declaration)
+    {
+        _filter = filter;
+        Declaration = declaration;
+    }
+
+    public IEntity Entity
+    {
+        get
+        {
+            if (_filter.Count != 1) throw new Exception("Singleton filter contains more than one entity, or no entity");
+            return _filter.Get(0);
+        }
+    }
+}
